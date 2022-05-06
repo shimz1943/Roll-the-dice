c777029c9033f25350202535dfaa819abafe40e7
simeb <sime.budisa@live.com>
2022-05-06 16:45:36 +0200

    v0.0.1

--- app.py
+++ app.py
@@ -1,29 +1,88 @@
 import random
 
 
-def logika():
-    if obrana[0] >= napad[0] and obrana[1] >= napad[1]:
+#   Dice comparison and winner announcer function
+def dice_logic():
+    if dicenumber_off == 3 and dicenumber_def == 2:
 
-        print("Obrana dobija oba")
+        if defence[0] >= offence[0] and defence[1] >= offence[1]:
+            print("Offence lose both units")
 
-    elif napad[0] > obrana[0] and napad[1] > obrana[1]:
-        print("Napad dobija oba")
+        elif offence[0] > defence[0] and offence[1] > defence[1]:
+            print("Defence lose both units")
 
-    else:
-        print("Oba nose po 1")
+        else:
+            print("Both lose one unit")
+
+    if dicenumber_off == 2 and dicenumber_def == 2:
+
+        if defence[0] >= offence[0] and defence[1] >= offence[1]:
+            print("Offence lose both units")
+
+        elif offence[0] > defence[0] and offence[1] > defence[1]:
+            print("Defence lose both units")
+
+        else:
+            print("Both lose one unit")
+
+    if dicenumber_def == 1:
+        if defence[0] >= offence[0]:
+            print("Offence lose one unit")
+
+        elif offence[0] > defence[0]:
+            print("Defence lose one unit")
 
 
-napad = [str(random.randint(1, 6)), str(random.randint(1, 6))]
-obrana = [str(random.randint(1, 6)), str(random.randint(1, 6))]
+#   Initializing and declaring variables for lists with random numbers and no-value variables for dice input
+offence = [str(random.randint(1, 6)), str(random.randint(1, 6)), str(random.randint(1, 6))]
+defence = [str(random.randint(1, 6)), str(random.randint(1, 6))]
+dicenumber_off = None
+dicenumber_def = None
 
-napad.sort(reverse=True)
-obrana.sort(reverse=True)
+#   sorting lists - descending
+offence.sort(reverse=True)
+defence.sort(reverse=True)
 
-print("Napad: " + napad[0] + " " + napad[1])
-print("Obrana: " + obrana[0] + " " + obrana[1])
-print("--------------")
-logika()
+#   loop to make sure that player input is valid
+while True:
+    try:
+        dicenumber_off = int(input("Choose dice number for strike (1-3): "))
+        if dicenumber_off <= 3:
+            break
+
+    except ValueError:
+        if dicenumber_off > 3:
+            continue
+        continue
+
+while True:
+    try:
+        dicenumber_def = int(input("Choose dice number for defence (1-2): "))
+        if dicenumber_def <= 2:
+            break
+
+    except ValueError:
+        if dicenumber_def > 2:
+            continue
+        continue
+
+#  Rolled dice visual
+if dicenumber_off == 3:
+    print("Offence: " + offence[0] + " " + offence[1] + " " + offence[2])
 
 
+elif dicenumber_off == 2:
+    print("Offence: " + offence[0] + " " + offence[1])
 
 
+elif dicenumber_off == 1:
+    print("Offence: " + offence[0])
+
+if dicenumber_def == 2:
+    print("Defence: " + defence[0] + " " + defence[1])
+
+elif dicenumber_def == 1:
+    print("Defence: " + defence[0])
+
+print("------------")
+dice_logic()
